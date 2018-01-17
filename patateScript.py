1b6cfbdfce117c5957b59b4a93af0e0aa03dd103
eparisot <eparisot@student.42.fr>
2018-01-17 11:18:25 +0100

    keys test srcipt

--- patateScript.py
+++ patateScript.py
@@ -1,11 +1,11 @@
 from time import sleep
-import msvcrt
 import RPi.GPIO as GPIO
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 #debug####
-#import cv2
+import cv2
 #####################################
+
 #init GPIO with BCM numberings
 GPIO.setmode(GPIO.BCM)
 
@@ -24,38 +24,60 @@ MOT1b = 13
 MOT2v = GPIO.PWM(16, 50)
 MOT2f = 21
 MOT2b = 20
+speeds = [0, 0]
 
-def Forward(speed):
-  GPIO.output(MOT1b, GPIO.LOW)
-  GPIO.output(MOT2b, GPIO.LOW)
-  GPIO.output(MOT1f, GPIO.HIGH)
-  GPIO.output(MOT2f, GPIO.HIGH)
-  MOT1v.start(speed)
-  MOT2v.start(speed)
-  return (speed)
-  
-def Brake(speed, delta):
-  MOT1v.ChangeDutyCycle(speed - delta)
-  MOT2v.ChangeDutyCycle(speed - delta)
-  return (speed - delta)
+def Forward(speeds, delta):
+    if speeds[0] != speeds[1]:
+        if speeds[0] > speeds[1]:
+            speeds[1] = speeds[0]
+        else:
+            speeds[0] = speeds[1]
+    elif speeds[0] + delta <= 100.0 and speeds[1] + delta <= 100.0:
+        speeds[0] = speeds[0] + delta
+        speeds[1] = speeds[1] + delta
+    GPIO.output(MOT1b, GPIO.LOW)
+    GPIO.output(MOT2b, GPIO.LOW)
+    GPIO.output(MOT1f, GPIO.HIGH)
+    GPIO.output(MOT2f, GPIO.HIGH)
+    MOT1v.start(speeds[0])
+    MOT2v.start(speeds[1])
+    return (speeds)
+
+def Brake(speeds, delta):
+    if speeds[0] > delta and speeds[1] > delta:
+        speeds[0] = speeds[0] - delta
+        speeds[1] = speeds[1] - delta
+    MOT1v.ChangeDutyCycle(speeds[0])
+    MOT2v.ChangeDutyCycle(speeds[1])
+    return (speeds)
 
 def Stop():
-  MOT1v.stop()
-  MOT2v.stop()
-  
-def TurnR(speed, ldelta, rdelta):
-  MOT1v.ChangeDutyCycle(speed + ldelta)
-  MOT2v.ChangeDutyCycle(speed - rdelta)
-  
-def TurnL(speed, ldelta, rdelta):
-  MOT1v.ChangeDutyCycle(speed - ldelta)
-  MOT2v.ChangeDutyCycle(speed + rdelta)
+    MOT1v.stop()
+    MOT2v.stop()
+    return ([0, 0])
 
+def TurnR(speeds, delta):
+    if speeds[0] + delta < 100.0:
+        speeds[0] = speeds[0] + delta
+        MOT1v.ChangeDutyCycle(speeds[0])
+    elif speeds[1] - delta > 0.0:
+        speeds[1] = speeds[1] - delta
+        MOT2v.ChangeDutyCycle(speeds[1])
+    return (speeds)
+
+def TurnL(speeds, delta):
+    if speeds[1] + delta < 100.0:
+        speeds[1] = speeds[1] + delta
+        MOT2v.ChangeDutyCycle(speeds[1])
+    elif speeds[0] - delta > 0.0:
+        speeds[0] = speeds[0] - delta
+        MOT1v.ChangeDutyCycle(speeds[0])
+    return (speeds)
 
 # Video here ############################################
 camera = PiCamera()
 camera.resolution = (320, 240)
-camera.framerate = 30
+camera.framerate = 60
 camera.hflip = True
 camera.vflip = True
 rawCapture = PiRGBArray(camera, size=(320, 240))
@@ -63,51 +85,31 @@ rawCapture = PiRGBArray(camera, size=(320, 240))
 sleep(0.1)
 
 ### Capture frames examples
-##for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 ##  # grab Numpy Array
-##  img = frame.array
+  img = frame.array
 ##  # Process images here
 ##  # show the frame
-##  cv2.imshow("Frame", img)
-##  key = cv2.waitKey(1) & 0xFF
+  cv2.imshow("Frame", img)
+  key = cv2.waitKey(1) & 0xFF
 ##  # Clear the stream
-##  rawCapture.truncate(0)
+  rawCapture.truncate(0)
 ##  # if 'q' key pressed, break loop
-##  if key == ord("q"):
-##    break
-
-# Wait for start
-print("Press 's' to Start !...")
-msvcrt.getch()
+  if key == ord("p"):
+    break
+# Control example :
+  elif key == ord("z"):
+    speeds = Forward(speeds, 20)
+  elif key == ord("s"):
+    speeds = Brake(speeds, 20)
+  elif key == ord("d"):
+    speeds = TurnR(speeds, 20)
+  elif key == ord("q"):
+    speeds = TurnL(speeds, 20)
 
 # IA here ##############################################
-# Control example :
-##speed = Forward(speed)
-##sleep(1)
-##Brake(speed, 20)
-##sleep(1)
-##Forward(speed)
-##sleep(1)
-##TurnR(speed, 20, 10)
-##sleep(1)
-##TurnL(speed, 10, 20)
-##sleep(1)
-##Stop()
 
-while True:
-    keycode = ord(msvcrt.getch())
-    if keycode == 27: #ESC
-        break
-    elif keycode == 224: #Special keys (arrows, f keys, ins, del, etc.)
-        keycode = ord(mscvrt.getch())
-        if keycode == 80: #Down arrow
-            speed = Brake(speed, 20)
-        elif keycode == 72: #Up arrow
-            speed = Forward(50)
-        elif keycode == : #Left arrow
-            TurnL(speed, 20)
-        elif keycode == : #Right arrow
-            TurnR(speed, 20)
+
 
 #Stop the machine and release GPIO Pins#################
 Stop()
