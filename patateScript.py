5b2dd1426090d0b59486021cabfb0c552b1c49f9
Klhnikov Riton <parisot.eric.mail@gmail.com>
2018-01-31 18:06:17 +0100

    colors ON

--- patateScript.py
+++ patateScript.py
@@ -3,9 +3,6 @@ import RPi.GPIO as GPIO
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from keras.models import load_model
-from PIL import Image
-#debug####
-import cv2
 #####################################
 
 # Load Model:
@@ -33,11 +30,11 @@ speeds = [0, 0]
 
 # Video here ############################################
 camera = PiCamera()
-camera.resolution = (320, 240)
+camera.resolution = (160, 128)
 camera.framerate = 60
 camera.hflip = True
 camera.vflip = True
-rawCapture = PiRGBArray(camera, size=(320, 240))
+rawCapture = PiRGBArray(camera, size=(160, 128))
 # Allow cam to warmup
 sleep(0.1)
 
@@ -48,27 +45,21 @@ MOT2V.start(0)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 ##  # grab Numpy Array
   img = frame.array
-##  # convert to gray
-  img = Image.fromarray(img, 'L')
 ##  # normalize
   img /= 255
   #IA here:
   image = []
   image.insert(0, img)
   preds = model.predict(image)
-  v1 = (np.argmax(preds[0], axis=1) + 1) * 10 
+  v1 = (np.argmax(preds[0], axis=1) + 1) * 10
   v2 = (np.argmax(preds[1], axis=1) + 1) * 10
   MOT1v.ChangeDutyCycle(v1)
   MOT2v.ChangeDutyCycle(v2)
-##  # show the frame
-  cv2.imshow("Frame", img)
-  key = cv2.waitKey(1) & 0xFF
+  print("L = " + v1 + " - R = " + v2)
 ##  # Clear the stream
   image.pop(0)
   rawCapture.truncate(0)
-##  # if 'q' key pressed, break loop
-  if key == ord("q"):
-    break
+
 
 MOT1v.stop(0)
 MOT2V.stop(0)
