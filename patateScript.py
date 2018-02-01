e54df0c7d7c43395f1c3df4dce5d2161bb72794b
Klhnikov Riton <parisot.eric.mail@gmail.com>
2018-02-01 16:23:38 +0100

    loop start

--- patateScript.py
+++ patateScript.py
@@ -8,6 +8,7 @@ import numpy as np
 
 # Load Model:
 model = load_model('test_model.h5')
+print("Model Loaded")
 
 #init GPIO with BCM numberings
 GPIO.setmode(GPIO.BCM)
@@ -36,34 +37,38 @@ camera.hflip = True
 camera.vflip = True
 rawCapture = PiRGBArray(camera, size=(160, 128))
 
+# Start loop
+print("Ready !...")
+try:
+  while True:
+    pass
+except KeyboardInterrupt:
+  pass
 
 MOT1v.start(0)
 MOT2v.start(0)
 
+try:
 ### Capture frames examples
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+  for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 ##  # grab Numpy Array
-  img = frame.array
-  #debug
-  #import png
-  #png.from_array(img, 'L').save("test.png")
-  ######
-  image = np.array([img[:, :, :]])
-  image = image.transpose(0, 2, 1, 3)
-  preds = model.predict(image)
-  v1 = (np.argmax(preds[0], axis=1) + 1) * 10
-  v2 = (np.argmax(preds[1], axis=1) + 1) * 10
-  MOT1v.ChangeDutyCycle(v1)
-  MOT2v.ChangeDutyCycle(v2)
-  print("L = " + str(v1) + " - R = " + str(v2))
+    img = frame.array
+    image = np.array([img[:, :, :]])
+    image = image.transpose(0, 2, 1, 3)
+    preds = model.predict(image)
+    v1 = 0.5 * (np.argmax(preds[0], axis=1) + 1) * 10
+    v2 = 0.5 * (np.argmax(preds[1], axis=1) + 1) * 10
+    MOT1v.ChangeDutyCycle(v1)
+    MOT2v.ChangeDutyCycle(v2)
+    print("L = " + str(v1) + " - R = " + str(v2))
 ##  # Clear the stream
-  image = np.delete(image, 0)
-  rawCapture.truncate(0)
-
+    image = np.delete(image, 0)
+    rawCapture.truncate(0)
+except KeyboardInterrupt:
+  pass
 
 MOT1v.stop(0)
 MOT2v.stop(0)
 
 #Stop the machine and release GPIO Pins#################
-Stop()
 GPIO.cleanup()
