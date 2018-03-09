ea456ce10d353c616ad989f3f9717f1b2dc93da1
Klhnikov Riton <parisot.eric.mail@gmail.com>
2018-03-09 18:28:59 +0100

    POW

--- patateScript.py
+++ patateScript.py
@@ -65,21 +65,21 @@ try:
         preds = 3
 ##  # Action
     if preds == 0:
-        speed = 8
+        speed = 7.5
         direction = 4
     elif preds == 1:
         image_a = np.array([img[40:58, :, :]])
         preds_a = np.argmax(model_a.predict(image_a), axis=1)
         if preds_a == 0:
-          speed = 8
+          speed = 7.5
         else:
-          speed = 8
+          speed = 7.5
         direction = 7
     elif preds == 2:
-        speed = 8
+        speed = 7.5
         direction = 10
     elif preds == 3:
-        speed = 8
+        speed = 7.5
         direction = 7
     POW.ChangeDutyCycle(tg - speed)
     DIR.ChangeDutyCycle(direction)
