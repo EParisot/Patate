badb10e3e57a13eac11ff2b11ffa4972b1845357
tim <tnicolas@student.42.fr>
2018-03-19 15:52:48 +0100

    new version of label

--- label/label.py
+++ label/label.py
@@ -4,16 +4,14 @@ from srcs.photos import Photos
 
 # python3 label.py photos_srcs photos_label trash 1
 
-if not len(sys.argv) in (3, 4, 5):
-    print('Usage: python3 label.py <photos> <labelised photos> [trash=\'trash\'] [auto_next=[01]]')
+if not len(sys.argv) in (2, 3):
+    print('Usage: python3 label.py <photos> [auto_next=[01]]')
     exit(0)
 
-if len(sys.argv) == 3:
-    p = Photos(sys.argv[1] + '/', sys.argv[2] + '/')
-elif len(sys.argv) == 4:
-    p = Photos(sys.argv[1] + '/', sys.argv[2] + '/', sys.argv[3] + '/')
+if len(sys.argv) == 2:
+    p = Photos(sys.argv[1] + '/')
 else:
-    p = Photos(sys.argv[1] + '/', sys.argv[2] + '/', sys.argv[3] + '/', True if sys.argv[4] == '1' else False)
+    p = Photos(sys.argv[1] + '/', False if sys.argv[2] == '0' else True)
 
 p.load()
 p.init_win()
--- label/srcs/photos.py
+++ label/srcs/photos.py
@@ -5,10 +5,8 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 class Photos(object):
-    def __init__(self, photos, lab_photos, trash='trash/', auto_next=False, width=800, height=600):
+    def __init__(self, photos, auto_next=True, width=800, height=600):
         self.photos_dir = photos
-        self.lab_photos_dir = lab_photos
-        self.trash_dir = trash
         self.photo_act = 0
         self.fen = {
             'fen' : None,
@@ -24,8 +22,10 @@ class Photos(object):
 
     def load(self):
         self.photos = os.listdir(self.photos_dir)
-        self.photos_inf = [{'del' : False, 'label' : ''} for i in range(len(self.photos))]
         self.photo_act = 0
+        if len(self.photos) == 0:
+            print(c.RED + 'ERROR: ' + c.EOC + 'no photos')
+            exit(1)
 
     def init_win(self, width=0, height=0):
         self.width = width if width != 0 else self.width
@@ -39,52 +39,34 @@ class Photos(object):
 
     def init_key(self):
         self.fen['fen'].bind("<Escape>", self.quit_win)
-        self.fen['fen'].bind("<BackSpace>", self.del_label)
-        self.fen['fen'].bind("<Control-Key-s>", self.save)
         self.fen['fen'].bind("<Right>", self.next_photo)
         self.fen['fen'].bind("<Left>", self.last_photo)
         self.fen['fen'].bind("<Up>", self.del_photo)
         self.fen['fen'].bind("<KeyPress>", self.event_win)
 
-    def save(self, event):
-        for i in range(len(self.photos)):
-            if self.photos_inf[i]['del'] == True:
-                print(c.RED + 'DELETE -> ' + c.EOC + self.photos[i])
-                os.rename(self.photos_dir + self.photos[i], self.trash_dir + self.photos[i])
-            if self.photos_inf[i]['label'] != '':
-                print(c.GREEN + 'LABEL -> ' + c.EOC + self.photos_inf[i]['label'] + self.photos[i])
-                os.rename(self.photos_dir + self.photos[i], self.lab_photos_dir + self.photos_inf[i]['label'] + self.photos[i])
-        self.load()
-
     def quit_win(self, event):
-        self.save(None)
         self.fen['fen'].destroy()
         self.fen['fen'].quit()
 
-    def del_label(self, event):
-        self.photos_inf[self.photo_act]['label'] = ''
-        if self.auto_next == True:
-            self.next_photo(None)
-        else:
-            self.print_win()
-
     def event_win(self, event):
         if event.char in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
-            self.photos_inf[self.photo_act]['label'] = event.char
+            self.set_label(event.char)
             if self.auto_next == True:
                 self.next_photo(None)
             else:
                 self.print_win()
 
     def del_photo(self, event):
-        if self.photos_inf[self.photo_act]['del'] == True:
-            self.photos_inf[self.photo_act]['del'] = False
-        else:
-            self.photos_inf[self.photo_act]['del'] = True
-        if self.auto_next == True:
-            self.next_photo(None)
-        else:
-            self.print_win()
+        print(c.RED + 'REMOVE: ' + c.EOC + self.photos[self.photo_act])
+        os.remove(self.photos_dir + self.photos[self.photo_act])
+        self.photos.pop(self.photo_act)
+        if len(self.photos) == 0:
+            print(c.MAGENTA + 'THE END' + c.EOC)
+            self.quit_win(None)
+            exit(0)
+        if self.photo_act >= len(self.photos):
+            self.photo_act = 0
+        self.print_win()
 
     def last_photo(self, event):
         self.photo_act -= 1
@@ -108,10 +90,29 @@ class Photos(object):
         self.fen['photo'] = ImageTk.PhotoImage(image)
         self.fen['lab_photo'] = Label(self.fen['fen'], image=self.fen['photo'])
         self.fen['lab_photo'].pack(side=TOP)
-        self.fen['lab_info'] = Label(self.fen['fen'], width=32,  height=2, font=("Courier", 40))
-        if self.photos_inf[self.photo_act]['del'] == True:
-            self.fen['lab_info'].configure(bg='red')
-        else:
-            self.fen['lab_info'].configure(bg='white')
-        self.fen['lab_info']['text'] = self.photos_inf[self.photo_act]['label']
+        self.fen['lab_info'] = Label(self.fen['fen'], width=32, height=2, font=("Courier", 40))
+        self.fen['lab_info']['text'] = self.get_label() + '\t\t' + str(self.photo_act) + '/' + str(len(self.photos))
         self.fen['lab_info'].pack(side=BOTTOM)
+        
+
+    def get_label(self):
+        try:
+            char = self.photos[self.photo_act].split('_')[0]
+            if len(char) == 1 and char.isdigit():
+                return char
+        except:
+            pass
+        return ''
+
+    def set_label(self, label):
+        new = ''
+        if self.get_label() == '':
+            if self.photos[self.photo_act][0] == '_':
+                new = label + self.photos[self.photo_act]
+            else:
+                new = label + '_' + self.photos[self.photo_act]
+        else:
+            new =  label + '_' + self.photos[self.photo_act].split('_')[1:][0]
+        print(c.GREEN + 'RENAME: ' + c.EOC + new)
+        os.rename(self.photos_dir + self.photos[self.photo_act], self.photos_dir + new)
+        self.photos[self.photo_act] = new
